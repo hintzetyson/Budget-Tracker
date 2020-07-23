1e16b488964f8e768692313987d4be45869e0476
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-23 15:03:14 -0600

    Added onlinecheck, onerror, onupgradeneeded

--- public/db.js
+++ public/db.js
@@ -2,6 +2,19 @@ let db;
 
 window.addEventListener('online', onlineCheck)
 
+const index = indexedDB.open('budget', 1);
+
+//runs if an upgrade is needed
+index.onupgradeneeded = event => {
+    const db = event.target.result;
+    db.createObjectStore('pending', {autoIncrement: true});
+}
+
+//Runs if there is an error
+index.onerror = event => {
+    console.log(`There was an error with the code: ${event.target.errorcode}`)
+}
+
 function onlineCheck() {
     const transaction = db.transaction(['pending'], 'readwrite');
 
@@ -9,7 +22,31 @@ function onlineCheck() {
 
     const getAll = store.getAll();
 
+    //This is what to do if there is actually something within indexedDB
     getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*', 'Content-Type': 'application/json'
+                }
 
+            }).then(res => {res.json()}).then(() => {
+                const transaction = db.transaction(['pending'], 'readwrite');
+
+                const store = transaction.objectStore('pending');
+
+                store.clear()
+            })
+        }
     }
+}
+
+function saveData (data) {
+    const transaction = db.transaction(['pending'], 'readwrite');
+
+    const store = transaction.objectStore('pending');
+
+    store.add(data)
 }
