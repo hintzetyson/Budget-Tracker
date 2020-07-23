d9b203c0a4ea90cb0c04500832e673f84d34b6f6
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-07-23 15:12:18 -0600

    Formatted files

--- public/db.js
+++ public/db.js
@@ -1,61 +1,65 @@
 let db;
 
-window.addEventListener('online', onlineCheck)
+window.addEventListener("online", onlineCheck);
 
-const index = indexedDB.open('budget', 1);
+const index = indexedDB.open("budget", 1);
 
 //runs if an upgrade is needed
-index.onupgradeneeded = event => {
-    const db = event.target.result;
-    db.createObjectStore('pending', {autoIncrement: true});
-}
+index.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
 
 //Runs if there is an error
-index.onerror = event => {
-    console.log(`There was an error with the code: ${event.target.errorcode}`)
-}
+index.onerror = (event) => {
+  console.log(`There was an error with the code: ${event.target.errorcode}`);
+};
 
-index.onsuccess = event => {
-    db = event.target.result;
+index.onsuccess = (event) => {
+  db = event.target.result;
 
-    if (navigator.onLine) {
-        //Looks in the database to see if anything is online
-        onlineCheck()
-    }
-}
+  if (navigator.onLine) {
+    //Looks in the database to see if anything is online
+    onlineCheck();
+  }
+};
 
 function onlineCheck() {
-    const transaction = db.transaction(['pending'], 'readwrite');
+  const transaction = db.transaction(["pending"], "readwrite");
 
-    const store = transaction.objectStore('pending');
+  const store = transaction.objectStore("pending");
 
-    const getAll = store.getAll();
+  const getAll = store.getAll();
 
-    //This is what to do if there is actually something within indexedDB
-    getAll.onsuccess = () => {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*', 'Content-Type': 'application/json'
-                }
+  //This is what to do if there is actually something within indexedDB
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          res.json();
+        })
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
 
-            }).then(res => {res.json()}).then(() => {
-                const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore("pending");
 
-                const store = transaction.objectStore('pending');
-
-                store.clear()
-            })
-        }
+          store.clear();
+        });
     }
+  };
 }
 
-function saveData (data) {
-    const transaction = db.transaction(['pending'], 'readwrite');
+function saveData(data) {
+  const transaction = db.transaction(["pending"], "readwrite");
 
-    const store = transaction.objectStore('pending');
+  const store = transaction.objectStore("pending");
 
-    store.add(data)
+  store.add(data);
 }
