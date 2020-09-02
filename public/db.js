95988776138be18fb3a46aca01bd4bcc737b7cff
hintzetyson <46944160+hintzetyson@users.noreply.github.com>
2020-09-02 11:04:54 -0600

    Added eslint

--- public/db.js
+++ public/db.js
@@ -1,16 +1,16 @@
 let db;
 
-window.addEventListener("online", onlineCheck);
+window.addEventListener('online', onlineCheck);
 
-const index = indexedDB.open("budget", 1);
+const index = indexedDB.open('budget', 1);
 
-//runs if an upgrade is needed
+// runs if an upgrade is needed
 index.onupgradeneeded = (event) => {
   const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+  db.createObjectStore('pending', { autoIncrement: true });
 };
 
-//Runs if there is an error
+// Runs if there is an error
 index.onerror = (event) => {
   console.log(`There was an error with the code: ${event.target.errorcode}`);
 };
@@ -19,36 +19,36 @@ index.onsuccess = (event) => {
   db = event.target.result;
 
   if (navigator.onLine) {
-    //Looks in the database to see if anything is online
+    // Looks in the database to see if anything is online
     onlineCheck();
   }
 };
 
 function onlineCheck() {
-  const transaction = db.transaction(["pending"], "readwrite");
+  const transaction = db.transaction(['pending'], 'readwrite');
 
-  const store = transaction.objectStore("pending");
+  const store = transaction.objectStore('pending');
 
   const getAll = store.getAll();
 
-  //This is what to do if there is actually something within indexedDB
+  // This is what to do if there is actually something within indexedDB
   getAll.onsuccess = () => {
     if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
         },
       })
         .then((res) => {
           res.json();
         })
         .then(() => {
-          const transaction = db.transaction(["pending"], "readwrite");
+          const transaction = db.transaction(['pending'], 'readwrite');
 
-          const store = transaction.objectStore("pending");
+          const store = transaction.objectStore('pending');
 
           store.clear();
         });
@@ -57,9 +57,9 @@ function onlineCheck() {
 }
 
 function saveData(data) {
-  const transaction = db.transaction(["pending"], "readwrite");
+  const transaction = db.transaction(['pending'], 'readwrite');
 
-  const store = transaction.objectStore("pending");
+  const store = transaction.objectStore('pending');
 
   store.add(data);
 }
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,52 +1,61 @@
+// Files to be cached for saving later
 const FILES_TO_CACHE = [
-    '/', 
-    '/index.html', 
-    '/index.js', 
-    '/styles.css', 
-    '/offline.js', 
-    '/manifest.webmanifest',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/index.js',
+  '/styles.css',
+  '/db.js',
+  '/manifest.webmanifest',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
 ];
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(PRECACHE).then(
-        cacheData => cacheData.addAll(FILES_TO_CACHE)
-    ).then(self.skipWaiting()));
+// Adds the files on an install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cacheData) => cacheData.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting()),
+  );
 });
 
-self.addEventListener('activate', event => {
-    const mainCachesNames = [PRECACHE, RUNTIME]
+self.addEventListener('activate', (event) => {
+  const mainCachesNames = [PRECACHE, RUNTIME];
 
-    event.waitUntil(caches.keys().then(cacheNames => {
-        return cacheNames.filter(cachedName => !mainCachesNames.includes(cachedName));
-    }).then(deleteCaches => {
-        return Promise.all(deleteCaches.map(deleteThisCache => {
-            return caches.delete(deleteThisCache);
-        }));
-    }).then(() => {
-        return self.clients.claim();
-    }));
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => cacheNames.filter(
+        (cachedName) => !mainCachesNames.includes(cachedName),
+      ))
+      .then((deleteCaches) => Promise.all(
+        deleteCaches.map((deleteThisCache) => caches.delete(deleteThisCache)),
+      ))
+      .then(() => self.clients.claim()),
+  );
 });
 
-self.addEventListener('fetch', data => {
-    if (data.request.url.includes('/api/')) {
-        data.respondWith(
-            caches.open(RUNTIME).then(cache => {
-                return fetch(data.request).then(response => {
-                    if (response.status === 200) {
-                        cache.put(data.request, response.clone())
-                    }
-                    return response;
-                }).catch(err => {
-                    return cache.match(data.request);
-                })
-            }).catch(err => {
-                console.log(err);
-            })
-        )
-    }
+// Gets the files after running
+self.addEventListener('fetch', (data) => {
+  if (data.request.url.includes('/api/')) {
+    data.respondWith(
+      caches
+        .open(RUNTIME)
+        .then((cache) => fetch(data.request)
+          .then((response) => {
+            if (response.status === 200) {
+              cache.put(data.request, response.clone());
+            }
+            return response;
+          })
+          .catch((err) => cache.match(data.request)))
+        .catch((err) => {
+          console.log(err);
+        }),
+    );
+  }
 });
